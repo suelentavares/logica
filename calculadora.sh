8c4cd1a30b65a8a4b39b979fe871fcb8c8bc7bb7
suelentavares <suelentavares9@gmail.com>
2024-10-29 16:17:12 -0300

    Create calculadora.sh
    create  file calculadora

---
+++ calculadora.sh
@@ -0,0 +1,21 @@
+#calculadora
+
+num1 = 0
+num2  = 0
+ result = 0
+ op  = ''
+ whele true ;
+ num1 = float (input('Digite o primeiro número : '))
+op = input('dite a peração')
+num2 = float (input('Digite o segundo'))
+
+  if  op == '+':
+     result = num1 + num2
+  elif oop == '-':
+     result = num1 - num2
+  elif op == '*':
+     result == num1 * num2
+  elif op == '/':
+      result == num1 / num2
+    else:
+      print( '{} {} {} = {}' .format(num1, op , num2 result))num1 = 0
